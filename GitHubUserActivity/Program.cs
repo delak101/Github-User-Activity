583b0be4a6c687d97f2692bf29758ca82f415ada
Khaled Hesham <khaledhesham007@gmail.com>
2024-10-12 06:05:49 +0300

    init commit

--- GitHubUserActivity/Program.cs
+++ GitHubUserActivity/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string username = args[0];
+        string url = $"https://api.github.com/users/{username}/events";
+
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+
+            try {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseBody);
+            } catch (HttpRequestException e) {
+                Console.WriteLine($"Request failed: {e.Message}");
+            }
+
+        }
+    }
+}
